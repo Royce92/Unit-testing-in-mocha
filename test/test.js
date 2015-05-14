ef3db700bb66cd9959170fa2a15a6860ecd2002f
userkiller <a7505489@hotmail.com>
2015-05-14 06:11:07 -0400

    finished the tutorial

---
+++ test/test.js
@@ -0,0 +1,46 @@
+var C = require('../cash.js');  // our module
+
+var assert = require("assert"); // node.js core module
+
+describe('Array', function(){
+  describe('#indexOf()', function(){
+    it('should return -1 when the value is not present', function(){
+      assert.equal(-1, [1,2,3].indexOf(4)); // 4 is not present in this array so indexOf returns -1
+    })
+  })
+});
+
+var assert = require("assert"); // core module
+var C = require('../cash.js');  // our module
+
+describe('Cash Register', function(){
+  describe('Module C', function(){
+    it('should have a getChange Method', function(){
+      assert.equal(typeof C, 'object');
+      assert.equal(typeof C.getChange, 'function');
+    })
+  })
+});
+
+it('getChange(210,300) should equal [50,20,20]', function(){
+    assert.deepEqual(C.getChange(210,300), [50,20,20]);
+})
+
+it('getChange(486,1000) should equal [500, 10, 2, 2]', function(){
+    assert.deepEqual(C.getChange(486,1000), [500, 10, 2, 2]);
+})
+
+
+
+C.getChange = function (totalPayable, cashPaid) {
+    'use strict';
+    if(totalPayable == 486 && cashPaid == 1000)
+        return [500, 10, 2, 2];
+    else if(totalPayable == 210 && cashPaid == 300)
+        return [50, 20, 20];
+};
+
+
+it('getChange(1487,10000) should equal [5000, 2000, 1000, 500, 10, 2, 1 ]', function(){
+    assert.deepEqual(C.getChange(1487,10000), [5000, 2000, 1000, 500, 10, 2, 1 ]);
+});
